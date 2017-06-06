8a2c658ffa0a31ed633c0b00767fa9a0515a3f7c
dokano <daisuke.okano@soliton.co.jp>
2017-06-06 11:16:15 +0900

    Update Example todo-app

--- example/todo-app/src/App.js
+++ example/todo-app/src/App.js
@@ -1,23 +1,11 @@
 import React, {Component} from 'react';
-
-const Style = {
-  done: {
-    textDecoration: 'line-through',
-    opacity: 0.5
-  }
-}
-
-const FilterType = {
-  ALL: 'ALL',
-  DONE: 'DONE',
-  UNDONE: 'UNDONE'
-}
+import Form from './components/Form';
+import Table from './components/Table';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '',
       todos: [
         {
           id: 1,
@@ -35,47 +23,32 @@ class App extends Component {
           done: false,
           disabled: false
         }
-      ],
-      filterType: FilterType.UNDONE
+      ]
     }
-    this._renderTodo = this._renderTodo.bind(this);
     this._onDone = this._onDone.bind(this);
-    this._onChange = this._onChange.bind(this);
     this._onSubmit = this._onSubmit.bind(this);
-    this._getTodos = this._getTodos.bind(this);
-    this._onFilter = this._onFilter.bind(this);
     this._onDelete = this._onDelete.bind(this);
     this._todosToMap = this._todosToMap.bind(this);
   }
 
-  _onChange(e) {
-    this.setState({title: e.target.value});
+  _onSubmit(todo) {
+    const {todos} = this.state;
+    const newTodos = todos.concat([Object.assign({}, todo, {
+        id: (todos.length + 1)
+      })]);
+    this.setState({todos: newTodos});
   }
 
-  _onSubmit(e) {
-    e.preventDefault();
-    const {todos, title} = this.state;
-    const newTodos = todos.concat([
-      {
-        id: (todos.length + 1),
-        title: this.state.title,
-        done: false,
-        disabled: false
-      }
-    ]);
-    this.setState({title: '', todos: newTodos});
-  }
-
-  _onDone(e) {
-    const todos = this._todosToMap(e.target.value, (todo) => {
+  _onDone(value) {
+    const todos = this._todosToMap(value, (todo) => {
       todo.done = !todo.done;
       return todo;
     });
     this.setState(todos);
   }
 
-  _onDelete(e) {
-    const todos = this._todosToMap(e.target.value, (todo) => {
+  _onDelete(value) {
+    const todos = this._todosToMap(value, (todo) => {
       todo.disabled = !todo.disabled;
       return todo;
     });
@@ -87,50 +60,8 @@ class App extends Component {
     return this.state.todos.filter(todo => todo.id !== Number(id)).concat(todos);
   }
 
-  _renderTodo(todo) {
-    if (todo.disabled) {
-      return null;
-    }
-    return (
-      <tr key={todo.id}>
-        <td><input
-          type="checkbox"
-          name="done"
-          checked={todo.done}
-          value={todo.id}
-          onChange={this._onDone}/></td>
-        <td style={todo.done
-          ? Style.done
-          : {}}>{todo.title}</td>
-        <td>
-          <button
-            value={todo.id}
-            onClick={this._onDelete}
-            className="btn btn-mini btn-danger">Delete</button>
-        </td>
-      </tr>
-    )
-  }
-
-  _getTodos() {
-    const {todos, filterType} = this.state;
-    switch (filterType) {
-      case FilterType.DONE:
-        return todos.filter(todo => todo.done === true);
-      case FilterType.UNDONE:
-        return todos.filter(todo => todo.done === false);
-      case FilterType.ALL:
-      default:
-        return todos;
-    }
-  }
-
-  _onFilter(e) {
-    this.setState({filterType: e.target.value});
-  }
-
   render() {
-    const {title, filterType} = this.state;
+    const {todos} = this.state;
     return (
       <div className="container">
         <div className="row">
@@ -138,58 +69,9 @@ class App extends Component {
             <blockquote>
               <h1>ADD TODO</h1>
             </blockquote>
-            <form className="form-inline">
-              <div className="form-group">
-                <input
-                  type="text"
-                  name="title"
-                  className="form-control"
-                  placeholder="Input title..."
-                  value={title}
-                  onChange={this._onChange}/>
-              </div>
-              <button className="btn btn-primary" onClick={this._onSubmit}>Submit</button>
-            </form>
+            <Form onSubmit={this._onSubmit}/>
             <hr/>
-            <blockquote>
-              <h2>TODO List&nbsp;
-                <span className="label label-info">{filterType}</span>
-                <div className="pull-right">
-                  <button
-                    value={FilterType.ALL}
-                    onClick={this._onFilter}
-                    className="btn btn-default btn-mini">ALL</button>
-                  <button
-                    value={FilterType.DONE}
-                    onClick={this._onFilter}
-                    className="btn btn-default btn-mini">DONE</button>
-                  <button
-                    value={FilterType.UNDONE}
-                    onClick={this._onFilter}
-                    className="btn btn-default btn-mini">UNDONE</button>
-                </div>
-              </h2>
-            </blockquote>
-            <table className="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Done?</th>
-                  <th>Title</th>
-                  <th>Control</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this._getTodos().sort((a, b) => {
-                  if (a.id < b.id) {
-                    return -1;
-                  }
-                  if (a.id > b.id) {
-                    return 1;
-                  }
-                  return 0;
-                }).map(todo => this._renderTodo(todo))}
-              </tbody>
-            </table>
+            <Table todos={todos} onDone={this._onDone} onDelete={this._onDelete}/>
           </div>
         </div>
       </div>
---
+++ example/todo-app/src/components/Table.jsx
@@ -0,0 +1,134 @@
+import React, {PropTypes, Component} from 'react';
+
+const Style = {
+  done: {
+    textDecoration: 'line-through',
+    opacity: 0.5
+  }
+}
+
+const FilterType = {
+  ALL: 'ALL',
+  DONE: 'DONE',
+  UNDONE: 'UNDONE'
+}
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterType: FilterType.UNDONE
+    }
+    this._onDone = this._onDone.bind(this);
+    this._onDelete = this._onDelete.bind(this);
+    this._onFilter = this._onFilter.bind(this);
+    this._getTodos = this._getTodos.bind(this);
+    this._renderTodo = this._renderTodo.bind(this);
+  }
+
+  _onDone(e) {
+    this.props.onDone(e.target.value);
+  }
+
+  _onDelete(e) {
+    this.props.onDelete(e.target.value);
+  }
+
+  _renderTodo(todo) {
+    if (todo.disabled) {
+      return null;
+    }
+    return (
+      <tr key={todo.id}>
+        <td><input
+          type="checkbox"
+          name="done"
+          checked={todo.done}
+          value={todo.id}
+          onChange={this._onDone}/></td>
+        <td style={todo.done
+          ? Style.done
+          : {}}>{todo.title}</td>
+        <td>
+          <button
+            value={todo.id}
+            onClick={this._onDelete}
+            className="btn btn-mini btn-danger">Delete</button>
+        </td>
+      </tr>
+    )
+  }
+
+  _getTodos() {
+    const {todos} = this.props;
+    const {filterType} = this.state;
+    switch (filterType) {
+      case FilterType.DONE:
+        return todos.filter(todo => todo.done === true);
+      case FilterType.UNDONE:
+        return todos.filter(todo => todo.done === false);
+      case FilterType.ALL:
+      default:
+        return todos;
+    }
+  }
+
+  _onFilter(e) {
+    this.setState({filterType: e.target.value});
+  }
+
+  render() {
+    const {filterType} = this.state;
+    return (
+      <div className="col-8">
+        <blockquote>
+          <h2>TODO List&nbsp;
+            <span className="label label-info">{filterType}</span>
+            <div className="pull-right">
+              <button
+                value={FilterType.ALL}
+                onClick={this._onFilter}
+                className="btn btn-default btn-mini">ALL</button>
+              <button
+                value={FilterType.DONE}
+                onClick={this._onFilter}
+                className="btn btn-default btn-mini">DONE</button>
+              <button
+                value={FilterType.UNDONE}
+                onClick={this._onFilter}
+                className="btn btn-default btn-mini">UNDONE</button>
+            </div>
+          </h2>
+        </blockquote>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Done?</th>
+              <th>Title</th>
+              <th>Control</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this._getTodos().sort((a, b) => {
+              if (a.id < b.id) {
+                return -1;
+              }
+              if (a.id > b.id) {
+                return 1;
+              }
+              return 0;
+            }).map(todo => this._renderTodo(todo))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+Table.propTypes = {
+  todos: PropTypes.array.isRequired,
+  onDone: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired
+}
+
+export default Table;
